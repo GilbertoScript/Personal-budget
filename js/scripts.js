9bfea780086999b0e966367bb213b37a0644b002
GilbertoASJ <alvesgilberto84@gmail.com>
2021-02-24 15:03:34 -0300

    Update js file, create cadastrarDespesa, function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function cadastrarDespesa() {
+	let ano = document.querySelector('#ano')
+	let mes = document.querySelector('#mes')
+	let dia = document.querySelector('#dia')
+	let tipo = document.querySelector('#tipo')
+	let descricao = document.querySelector('#descricao')
+	let valor = document.querySelector('#valor')
+
+	console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+}
